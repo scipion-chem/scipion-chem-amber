80b688ea25eb6065bb77b0c256b9850a9da7c596
Joaquin Algorta Bove <joaco.algorta@gmail.com>
2026-07-09 11:34:22 +0200

    test fix

--- amber/tests/tests.py
+++ amber/tests/tests.py
@@ -113,7 +113,7 @@ class TestAmberPrepareSystemLig(TestPrepareReceptor, TestExtractLigand):
         else:
             protPrepareS.inputSetOfMols.set(protPrepare)
             protPrepareS.inputSetOfMols.setExtended('outputSmallMolecules')
-            protPrepareS.inputLigand.set('SmallMolecule (g1_1uaz_RET-1_1 molecule)')
+            protPrepareS.inputLigand.set('SmallMolecule (g1_1uaz_RET_255-1_1 molecule)')
 
         cls.launchProtocol(protPrepareS)
         return protPrepareS
